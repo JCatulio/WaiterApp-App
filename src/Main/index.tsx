e5af837e4abc3546362200faeae38ede7a73d17a
Jonathan <82682806+JCatulio@users.noreply.github.com>
2022-12-19 20:23:56 -0300

    Desenvolvimento da UI Mobile com React Native e TS - PT3

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -12,19 +12,40 @@ import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu';
 import { Button } from '../components/Button';
 import { TableModal } from '../components/TableModal';
+import { Cart } from '../components/Cart';
+import { CartItem } from '../types/CartItem';
+import { products } from '../mocks/products';
 
 export function Main() {
   const [isTableModalVisible, setIsTableModalVisible] = useState(false);
   const [selectedTable, setSelectedTable] = useState('');
+  const [cartItems, setCartItems] = useState<CartItem[]>([
+    {
+      quantity: 1,
+      product: products[0],
+    },
+    {
+      quantity: 2,
+      product: products[1],
+    },
+  ]);
 
   function handleSaveTable(table: string) {
     setSelectedTable(table);
+    setIsTableModalVisible(false);
+  }
+
+  function handleCancelOrder() {
+    setSelectedTable('');
   }
 
   return (
     <>
       <Container>
-        <Header />
+        <Header
+          selectedTable={selectedTable}
+          onCancelOrder={handleCancelOrder}
+        />
 
         <CategoriesContainer>
           <Categories />
@@ -42,6 +63,8 @@ export function Main() {
               Novo Pedido
             </Button>
           )}
+
+          {selectedTable && <Cart cartItems={cartItems} />}
         </FooterContainer>
       </Footer>
 
